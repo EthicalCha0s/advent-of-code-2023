0435a71940a8adda3ff26f2798d87fbb36fd8c26
EthicalChaos <boris.descubes@gmail.com>
2023-12-05 19:59:30 +0000

    Improved day 1 for part 2

--- day1/main.go
+++ day1/main.go
@@ -6,22 +6,45 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-// // Define a dictionary for text-to-digit mappings
-// var textToDigitMap = map[string]int{
-// 	"one":   1,
-// 	"two":   2,
-// 	"three": 3,
-// 	"four":  4,
-// 	"five":  5,
-// 	"six":   6,
-// 	"seven": 7,
-// 	"eight": 8,
-// 	"nine":  9,
-// }
+// Define a dictionary for text-to-digit mappings
+var textToDigitMap = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func getKeys(dict map[string]int) []string {
+	keys := []string{}
+	for key := range dict {
+		keys = append(keys, key)
+	}
+	return (keys)
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
 
 func stringToInt(text string) int {
+	textDigits := getKeys(textToDigitMap)
+	if stringInSlice(text, textDigits) {
+		return textToDigitMap[text]
+	}
+
 	i, err := strconv.Atoi(text)
 	if err != nil {
 		panic(err)
@@ -30,19 +53,17 @@ func stringToInt(text string) int {
 }
 
 func extractDigits(text string) int {
-	digitsRegex := regexp.MustCompile(`\d`)
+	textDigits := getKeys(textToDigitMap)
+	regexPattern := `((\d)|` + strings.Join(textDigits, "|") + ")"
+	digitsRegex := regexp.MustCompile(regexPattern)
 	digits := digitsRegex.FindAllString(text, -1)
 
-	if len(digits) == 1 {
-		return stringToInt(digits[0] + digits[0])
-	} else {
-		return stringToInt(digits[0] + digits[len(digits)-1])
-	}
+	return (stringToInt(digits[0]) * 10) + stringToInt(digits[len(digits)-1])
 }
 
 func main() {
 	// Open connection to text-file
-	filename := "input.txt"
+	filename := "input2.txt"
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
@@ -53,7 +74,8 @@ func main() {
 	sum := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		fmt.Println("\n" + line)
+		fmt.Println(extractDigits(line))
 
 		sum += extractDigits((line))
 	}
